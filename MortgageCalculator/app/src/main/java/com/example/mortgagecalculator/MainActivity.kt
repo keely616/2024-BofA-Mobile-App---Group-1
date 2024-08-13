60319266310865ca3b5312f1be33b12cdf10d0b8
parisgirl16 <bella.c.ott.04@gmail.com>
2024-08-13 15:24:36 -0500

    I'm trying, I promise

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.navigation.compose.NavHost
 import com.example.mortgagecalculator.R
 import com.example.mortgagecalculator.ui.theme.MortgageCalculatorTheme
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
 
@@ -63,6 +64,7 @@ class SecondActivity : ComponentActivity() {
 
     private lateinit var calculateButton3: Button
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -83,6 +85,9 @@ class SecondActivity : ComponentActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             // start the activity connect to the specified class
             startActivity(intent)
+
+            val homePrice = findViewById<EditText>(R.id.editPrice)
+            homePrice.setText("285000").toString().toDouble()
         }
 
         calculateButton2 = findViewById(R.id.goToCalculate2)
@@ -91,6 +96,9 @@ class SecondActivity : ComponentActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             // start the activity connect to the specified class
             startActivity(intent)
+
+            val homePrice = findViewById<EditText>(R.id.editPrice)
+            homePrice.setText("220000").toString().toDouble()
         }
 
         calculateButton3 = findViewById(R.id.goToCalculate3)
@@ -99,6 +107,9 @@ class SecondActivity : ComponentActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             // start the activity connect to the specified class
             startActivity(intent)
+
+            val homePrice = findViewById<EditText>(R.id.editPrice)
+            homePrice.setText("214973").toString().toDouble()
         }
     }
 }
@@ -109,37 +120,6 @@ class ThirdActivity : ComponentActivity() {
     private lateinit var homeButton: Button
 
     private lateinit var calculate: Button
-
-    private var price = findViewById<EditText>(R.id.editPrice)
-    private var interestRate = findViewById<EditText>(R.id.editInterest)
-    private var downpayment = findViewById<EditText>(R.id.editDownPayment)
-    private var loanAmount = findViewById<EditText>(R.id.editLoanAmount)
-    private var duration = findViewById<Spinner>(R.id.duration)
-    private var result = findViewById<TextView>(R.id.resultLabel)
-
-    private val p = price.text.toString().toDouble()
-    private val r = interestRate.text.toString().toDouble() / 100
-    private val dp = downpayment.text.toString().toDouble()
-    private var principal = p-dp
-    private val n = 12 * duration.selectedItem.toString().toDouble()
-
-    @SuppressLint("DefaultLocale")
-    val m = String.format("%.2f", (principal*r*(1+r)*n)/((1+r)*n-1)).toDouble()
-
-    private var textWatcher: TextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            // this function is called before text is edited
-        }
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            // this function is called when text is edited
-        }
-
-        override fun afterTextChanged(s: Editable) {
-            // this function is called after text is edited
-            loanAmount.setText(m.toString())
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -155,14 +135,51 @@ class ThirdActivity : ComponentActivity() {
             startActivity(intent)
         }
 
-        downpayment.addTextChangedListener(textWatcher)
+        val price = findViewById<EditText>(R.id.editPrice)
+        val interestRate = findViewById<EditText>(R.id.editInterest)
+        val downpayment = findViewById<EditText>(R.id.editDownPayment)
+        var loanAmount = findViewById<EditText>(R.id.editLoanAmount)
+        val duration = findViewById<Spinner>(R.id.duration)
+        val result = findViewById<TextView>(R.id.resultLabel)
+
+        val p = price.text.toString().toDouble()
+        val r = (interestRate.text.toString().toDouble() / 100) / 12
+        val dp = downpayment.text.toString().toDouble()
+        val principal = p-dp
+        loanAmount.setText(principal.toString())
+        val n = 12 * duration.selectedItem.toString().toDouble()
+        val i = (1+r).pow(n).toString().toDouble()
+
+        @SuppressLint("DefaultLocale")
+        val m = String.format("%.2f", (principal*r*i)/(i-1))
+
+//        downpayment.addTextChangedListener(textWatcher)
 
         calculate = findViewById(R.id.calculate)
 
         calculate.setOnClickListener {
-            result.text = m.toString()
+            result.text = m
         }
 
-
     }
+
+//    private var textWatcher: TextWatcher = object : TextWatcher {
+//
+//        val price = findViewById<EditText>(R.id.editPrice)
+//        val downpayment = findViewById<EditText>(R.id.editDownPayment)
+//        var loanAmount = findViewById<EditText>(R.id.editLoanAmount)
+//
+//        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+//            // this function is called before text is edited
+//        }
+//
+//        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+//            // this function is called when text is edited
+//        }
+//
+//        override fun afterTextChanged(s: Editable) {
+//            // this function is called after text is edited
+//            loanAmount.setText((price.text.toString().toDouble() - downpayment.text.toString().toDouble()).toString())
+//        }
+//    }
 }
